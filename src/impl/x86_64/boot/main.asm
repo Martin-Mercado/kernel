778d5d52b03400be32c7203ac3b81754989e5066
Martin-Mercado <mercadomartin364@gmail.com>
2023-12-10 20:20:21 -0300

    asm code to print "ok"

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,3 +4,8 @@ global  start
 section .text
 bits 32 ;porque todavia esta en modo 32 bits
 start:
+    ;imprimir "ok"
+    mov dword [0xb8000], 0x2f4b2f4f ;la memoria de video empiza en 0xb8000, 0x2f4b2f4f es OK
+
+    ;freeze
+    hlt
